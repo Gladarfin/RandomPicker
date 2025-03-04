76e11c04b7d8babb399858f72168c49a2f7e5a15
Gladarfin <gladarfinvs@gmail.com>
2025-03-04 16:00:55 +0500

    Merge ViewModels

--- RandomPicker.App/ViewModels/MainWindowViewModel.cs
+++ RandomPicker.App/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,50 @@
-﻿namespace RandomPicker.App.ViewModels;
+﻿using System.ComponentModel;
+
+namespace RandomPicker.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public double DropDownHeight { get; set; } = 150;
+    private bool _openFileAfterExit;
+    private bool _withoutRepetition;
+    private string _pathToFile = "Path not set";
+    
+    public bool OpenFileAfterExit
+    {
+        get => _openFileAfterExit;
+        set
+        {
+            if (_openFileAfterExit == value) return;
+            
+            _openFileAfterExit = value;
+            OnPropertyChanged(nameof(OpenFileAfterExit));
+        }
+    }
+    public bool WithoutRepetition
+    {
+        get => _withoutRepetition;
+        set
+        {
+            if (_withoutRepetition == value) return;
+            _withoutRepetition = value;
+            OnPropertyChanged(nameof(WithoutRepetition));
+        }
+    }
+    public string PathToFile
+    {
+        get => _pathToFile;
+        set
+        {
+            if (_pathToFile == value) return;
+            _pathToFile = value;
+            OnPropertyChanged(nameof(PathToFile));
+        }
+    }
+    
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
